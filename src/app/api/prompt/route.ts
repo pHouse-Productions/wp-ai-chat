e014a4ed5c4b42f4e632b9c6129a7170837980fa
Mike Carcasole <mikecarcasole@gmail.com>
2023-09-15 09:46:22 -0400

    Improve prompt. Slight speed expense but I think it's worth it

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -33,7 +33,17 @@ query AiRequests($id: ID!) {
 export async function POST(request: Request) {
   const body = ChatRequestSchema.parse(await request.json());
 
-  const transcript = body.messages.slice(-20);
+  const transcript = body.messages
+    .slice(-20)
+    .map((m) =>
+      `
+From: ${m.from}
+Body:
+${m.message}
+`.trim()
+    )
+    .join("\n\n");
+  const model = AiModel.Gpt_3_5Turbo;
 
   const url = process.env.WP_URL + "/graphql";
   const apiKey = process.env.WP_API_KEY;
@@ -49,31 +59,49 @@ mutation CreateAiRequest($input: AiRequestInput!) {
     variables: {
       input: {
         accountId,
-        dataSource: { type: "SACRA" },
-        prompt: {
-          type: "TEXT",
-          textContent: `
-CHAT_TRANSCRIPT_START
-${transcript
-  .map(({ from, message, date }) =>
-    `
-Date: ${date}
-From: ${from === "Bot" ? "You" : "User"}
-Body:
-${message}
-          `.trim()
-  )
-  .join("\n\n")}
-CHAT_TRANSCRIPT_END
-
-Respond using ONLY the data sources provided.
-If you cannot respond using the data sources provided, give the user some example questions they can ask you about the data sources.
-          `.trim(),
-        },
+        type: AiRequestType.DataSourcePrompt,
         includeReferences: true,
+        model,
+        temperature: 0,
+        dataSource: { type: "SACRA" },
         maxCharacters: 500,
-        model: "gpt_3_5_turbo",
-        type: "DATA_SOURCE_PROMPT",
+        prompt: {
+          type: DataSourceType.Array,
+          array: [
+            {
+              type: DataSourceType.AiRequest,
+              aiRequest: {
+                accountId,
+                type: AiRequestType.Prompt,
+                model,
+                prompt: {
+                  type: DataSourceType.Text,
+                  textContent: `
+START_TRANSCRIPT
+${transcript}
+END_TRANSCRIPT
+
+Rewrite the last message from "me" so it can be submitted as a stand-alone question or statement separate from the rest of the chat.
+It will be submitted to somebody that doesn't know anything about the transcript and has no context.
+Carry over any information that is relevant to the question or statement so it can be understood without context.
+Your response should look something like:
+"The user would like to know..."
+or
+"The user says that..."
+                `.trim(),
+                },
+              },
+            },
+            {
+              type: DataSourceType.Text,
+              textContent: `
+  
+Respond to the user using ONLY the data sources provided.
+If you cannot respond or if there's no specific query, suggest some topics that are in the data sources.
+              `.trimEnd(),
+            },
+          ],
+        },
       },
     },
   };
@@ -115,3 +143,26 @@ query apiKeys($apiKey: ID!) {
     )
     .parse(json.data.apiKeys)[0].accountId;
 };
+
+enum AiRequestType {
+  DataSourcePrompt = "DATA_SOURCE_PROMPT",
+  Facts = "FACTS",
+  Prompt = "PROMPT",
+  Summary = "SUMMARY",
+}
+
+enum DataSourceType {
+  AiRequest = "AI_REQUEST",
+  Array = "ARRAY",
+  GoogleSearch = "GOOGLE_SEARCH",
+  Sacra = "SACRA",
+  Text = "TEXT",
+  Url = "URL",
+}
+
+enum AiModel {
+  Gpt_3_5Turbo = "gpt_3_5_turbo",
+  Gpt_3_5Turbo_16k = "gpt_3_5_turbo_16k",
+  Gpt_4 = "gpt_4",
+  TextEmbeddingAda_002 = "text_embedding_ada_002",
+}
